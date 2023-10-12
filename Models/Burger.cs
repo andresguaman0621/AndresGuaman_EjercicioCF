37fdd3ca96a0c27b5ba915cdaeaacc1fc45a9875
andresguaman0621 <andresguaman0621@gmail.com>
2023-10-11 20:59:13 -0500

    segundo commit tabla
    segundo commit tabla

---
+++ Models/Burger.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndresGuaman_EjercicioCF.Models
+{
+    public class Burger
+    {
+        public int ID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 999.99)]
+        public decimal Price { get; set; }
+    }
+}
