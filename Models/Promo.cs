6dfbfae0aa25685824a802dbcdc759f9adeda53c
andresguaman0621 <andresguaman0621@gmail.com>
2023-10-18 08:35:32 -0500

    Inicio taller, clonación del proyecto, Creación clase Promo

---
+++ Models/Promo.cs
@@ -0,0 +1,12 @@
+﻿namespace AndresGuaman_EjercicioCF.Models
+{
+    public class Promo
+    {
+        public int PromoID { get; set; }
+        public string? PromoName { get; set; }
+        public string? PromoDescription { get; set; }
+        public int ID { get; set; }
+        public Burger? Burger { get; set; }
+    
+    }
+}
